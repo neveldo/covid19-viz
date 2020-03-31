916f45d80a2286672d4ab8047c8b0ede6d42bce5
neveldo <neveldo@gmail.com>
2020-03-31 21:21:59 +0200

    Refactor datasource module into a class

--- covid19/datasource.py
+++ covid19/datasource.py
@@ -1,32 +1,78 @@
 import pandas as pd
 
-"""
-Data module
-"""
-def get_data():
+class DataSource:
     """
-    Get covid-19 France data from data.gouv.fr
+    Data module
     """
-    data = pd.read_csv('https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv')
 
-    # Compute departments data
-    data_departments = data.copy()
-    data_departments = data_departments[(data_departments['granularite'] == 'departement')]
-    data_departments = _clean_data(data_departments)
-    data_departments['entity'] = data_departments['entity'].transform(lambda x: x[4:])
-    data_departments = data_departments.groupby('date').max()
-    data_departments = data_departments[pd.notna(data_departments['entity'])]
+    SOURCE_URL = 'https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv'
 
-    # Compute France data
-    data = data[(data['granularite'] == 'pays') & (data['maille_nom'] == 'France')]
-    data = _clean_data(data)
-    data = data.groupby('date').max()
+    def __init__(self):
+        self.data_country = pd.DataFrame()
+        self.data_departments = pd.DataFrame()
+        self.counters = {}
+        self.max_date = ''
 
-    return data, data_departments
 
-def _clean_data(data):
-    data = data.loc[:, ['date', 'maille_code', 'cas_confirmes', 'deces', 'reanimation', 'hospitalises', 'gueris']]
-    data.columns = ['date', 'entity', 'confirmed', 'death', 'resuscitation', 'hospitalized', 'healed']
-    data = data[data['date'] >= '2020-03-01']
+    def update_data(self):
+        """
+        Get covid-19 France data from data.gouv.fr
+        """
+        data = pd.read_csv(DataSource.SOURCE_URL)
 
-    return data
+        # Extract France data by day
+        self.data_country = data.copy()
+        self.data_country = self.data_country[(data['granularite'] == 'pays') & (self.data_country['maille_nom'] == 'France')]
+        self.data_country = DataSource._format_dataframe(self.data_country)
+        self.data_country = self.data_country.groupby('date').max()
+        self.data_country = self.data_country.sort_index()
+
+
+        columns = {
+            'confirmed_cumulative': 'confirmed',
+            'death_cumulative': 'death',
+            'resuscitation_cumulative': 'resuscitation',
+            'hospitalized_cumulative': 'hospitalized',
+            'healed_cumulative': 'healed',
+        }
+
+        self.max_date = self.data_country.index.max()
+
+        for column_cumulative_name, new_column_name in columns.items():
+            # Compute daily values instead from cumulative ones
+            self.data_country = DataSource._add_daily_data(self.data_country, column_cumulative_name, new_column_name)
+
+            # Compute most recent value for each feature
+            self.counters[new_column_name] = self.data_country.at[self.max_date, column_cumulative_name]
+
+        print(self.counters)
+
+        # #####################"
+
+        # Compute departments data
+        self.data_departments = data.copy()
+        self.data_departments = self.data_departments[(self.data_departments['granularite'] == 'departement')]
+        self.data_departments = DataSource._format_dataframe(self.data_departments)
+        self.data_departments['entity'] = self.data_departments['entity'].transform(lambda x: x[4:])
+        self.data_departments = self.data_departments.groupby('date').max()
+        self.data_departments = self.data_departments[pd.notna(self.data_departments['entity'])]
+
+    @staticmethod
+    def _format_dataframe(dataframe):
+        dataframe = dataframe.loc[:, ['date', 'maille_code', 'cas_confirmes', 'deces', 'reanimation', 'hospitalises', 'gueris']]
+        dataframe.columns = ['date', 'entity', 'confirmed_cumulative', 'death_cumulative', 'resuscitation_cumulative', 'hospitalized_cumulative', 'healed_cumulative']
+        dataframe = dataframe[dataframe['date'] >= '2020-03-01']
+
+        return dataframe
+
+    @staticmethod
+    def _add_daily_data(dataframe, column_cumulative_name, new_column_name):
+        cumulative_column = dataframe[column_cumulative_name].copy().reset_index(drop=True)
+        cumulative_column_previous_day = dataframe[column_cumulative_name].copy().reset_index(drop=True)
+        cumulative_column_previous_day = pd.Series([0]).append(cumulative_column_previous_day, ignore_index=True)
+        cumulative_column_previous_day = cumulative_column_previous_day[:-1]
+
+        dataframe[new_column_name] = list(cumulative_column - cumulative_column_previous_day)
+
+        return dataframe
+
